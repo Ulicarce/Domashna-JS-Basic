4c4c179b378e93002e367acb7f6a893151035d2c
Ulicarce <96683871+Ulicarce@users.noreply.github.com>
2022-12-10 18:03:49 +0100

    Add files via upload

---
+++ Domashna3-Part2/script.js
@@ -0,0 +1,12 @@
+function sayHello(){
+ console.log("Hello Pane & Radmila");
+}
+sayHello();
+
+let input = prompt("Please write dog's age");
+let numberInput = parseInt(input);
+console.log("Human year is:",numberInput / 7);
+
+let input1 = prompt("Please write Human year");
+let numberInput1 = parseInt(input1);
+console.log("Dog's year is:",7 * numberInput1);
