1ed7a06ad6b30885878b110d78fa196f377cb9fc
Ulicarce <96683871+Ulicarce@users.noreply.github.com>
2022-12-10 19:16:09 +0100

    Add files via upload

---
+++ Domashna3-Part 1/script.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+// object
+let myName = "Tatjana Ristovska";
+console.log("My name is", myName);
+
+// boolean
+let boolean = (10>6);
+console.log("10>6",boolean);
+
+// numbers
+let num = 38;
+console.log(`I have ${num} years old`);
+
+// string
+let text = "Java Script";
+console.log("Today we study",text);
+
+// undefined
+var x;
+console.log(x)
