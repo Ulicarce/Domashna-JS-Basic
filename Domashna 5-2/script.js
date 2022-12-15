b90880da1d9742a4d504876fd6226b0f8ac2d6ec
Ulicarce <96683871+Ulicarce@users.noreply.github.com>
2022-12-15 20:02:14 +0100

    Add files via upload

---
+++ Domashna 5-2/script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+
+function sumNumbers(number01, number02, number03, number04, number05) {
+ let result = (number01 + number02 + number03 + number04 + number05);
+ return result
+}
+
+let result = sumNumbers(2,5,10,3,6);
+console.log(`The sum of the numbers is ${result}`);
