5efb1b0a6b53ad1ebab3e4467ba8085fcf98136f
Ulicarce <96683871+Ulicarce@users.noreply.github.com>
2022-12-15 20:49:04 +0100

    Add files via upload

---
+++ Domashna 5 - 1/script.js
@@ -0,0 +1,6 @@
+console.log("Hello")
+var name1 = "Tanja";
+var name2 = "tired";
+var name3 = "writing";
+var name4 = "homework";
+console.log(`I'm ${name1}.${name1} is ${name2}  all day ${name3} ${name4} :-)`);
