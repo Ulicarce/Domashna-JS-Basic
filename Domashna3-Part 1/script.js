1f7026c096d68668d163161c08e10b4196b98ef8
Ulicarce <96683871+Ulicarce@users.noreply.github.com>
2022-12-25 18:39:08 +0100

    Update script.js

--- Domashna3-Part 1/script.js
+++ Domashna3-Part 1/script.js
@@ -1,20 +1,10 @@
 console.log("Hello");
-// object
-let myName = "Tatjana Ristovska";
-console.log("My name is", myName);
 
-// boolean
-let boolean = (10>6);
-console.log("10>6",boolean);
-
-// numbers
-let num = 38;
-console.log(`I have ${num} years old`);
-
-// string
-let text = "Java Script";
-console.log("Today we study",text);
-
-// undefined
-var x;
-console.log(x)
+function myFunction(y) {
+ return typeof y;
+}
+ console.log(myFunction(null));
+ console.log(myFunction(undefined));
+ console.log(myFunction("Hello SEDC"));
+ console.log(myFunction(true));
+ console.log(myFunction(15));
